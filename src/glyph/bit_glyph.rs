ab030308efeaedfc6d413a4f25db104bbd864479
Ryan Sullivan <ryansully96@googlemail.com>
2022-06-23 21:23:03 +0000

    Implement Display for BitGlyph

--- src/glyph/bit_glyph.rs
+++ src/glyph/bit_glyph.rs
@@ -1,7 +1,10 @@
 //! Basic glyph.
 //!
-
-use std::vec::Vec;
+use std::{
+    fmt::Display,
+    ops::{BitAnd, Not, Shl},
+    vec::Vec,
+};
 
 /// Glyph in a basic bitmap format where each bit represents a pixel.
 pub struct BitGlyph<const ROWS: usize, const COLS: usize>(Box<[u8]>);
@@ -10,12 +13,12 @@ impl<const R: usize, const C: usize> Default for BitGlyph<R, C> {
     /// Create a new Glyph with each pixel set to 0.
     ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use rust_binfont_utils::BitGlyph;
     /// let glyph: BitGlyph<16, 24> = BitGlyph::default();
     /// ```
-    /// 
+    ///
     fn default() -> Self {
         let data_length = {
             let cols = C as f64;
@@ -41,5 +44,30 @@ impl<const R: usize, const C: usize> BitGlyph<R, C> {
         } else {
             Some(Self(Box::from(data)))
         }
+    }
+
+    /// Return the number of bytes needed to represent all the bits in a row.
+    ///
+    /// # Safety
+    /// The result of ceil cannot be NaN, infinite or negative.
+    /// As the input is a usize / 8, we know this is the case.
+    ///
+    fn bytes_per_row() -> usize {
+        unsafe { (C as f64 / 8.0).ceil().to_int_unchecked() }
+    }
+}
+
+impl<const R: usize, const C: usize> Display for BitGlyph<R, C> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.chunks(Self::bytes_per_row()).for_each(|row| {
+            row.iter().for_each(|byte| {
+                let test: std::string::String = (0..8)
+                    .map(|shift| if byte & (128 >> shift) != 0 { '█' } else { '░' })
+                    .collect();
+                write!(f, "{}", test).unwrap();
+            });
+            writeln!(f).unwrap();
+        });
+        std::fmt::Result::Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use rust_binfont_utils::Glyph;
+use rust_binfont_utils::BitGlyph;
 
 fn main() {
-    Glyph::<8, 8>::new(&[0b00000000, 
-                         0b00000000,
-                         0b00000000,
-                         0b00000000,
-                         0b00000000,
-                         0b00000000,
-                         0b00000000,
-                         0b00000000]);
+    let glyph: BitGlyph<8, 8> = {
+        let r1 = 0b00011000;
+        let r2 = 0b01100110;
+        let r3 = 0b01100110;
+        let r4 = 0b01111110;
+        let r5 = 0b01100110;
+        let r6 = 0b01100110;
+        let r7 = 0b00000000;
+        let r8 = 0b11111111;
+        BitGlyph::new(&[r1, r2, r3, r4, r5, r6, r7, r8]).expect("Failed to create glpyh\n")
+    };
+
+    print!("{}", glyph);
 }
