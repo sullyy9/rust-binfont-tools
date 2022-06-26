18a91a39323b2c438c3ba8c1d9f1431a0c208ee6
Ryan Sullivan <ryansully96@googlemail.com>
2022-06-26 14:40:21 +0000

    Refactor with bitvec

--- src/glyph/bit_glyph.rs
+++ src/glyph/bit_glyph.rs
@@ -1,13 +1,11 @@
 //! Basic glyph.
 //!
-use std::{
-    fmt::Display,
-    ops::{BitAnd, Not, Shl},
-    vec::Vec,
-};
+use std::fmt::Display;
+
+use bitvec::prelude::*;
 
 /// Glyph in a basic bitmap format where each bit represents a pixel.
-pub struct BitGlyph<const ROWS: usize, const COLS: usize>(Box<[u8]>);
+pub struct BitGlyph<const ROWS: usize, const COLS: usize>(BitBox<u8, Lsb0>);
 
 impl<const R: usize, const C: usize> Default for BitGlyph<R, C> {
     /// Create a new Glyph with each pixel set to 0.
@@ -20,53 +18,27 @@ impl<const R: usize, const C: usize> Default for BitGlyph<R, C> {
     /// ```
     ///
     fn default() -> Self {
-        let data_length = {
-            let cols = C as f64;
-            let bytes_per_row = (cols / 8.0).ceil() as usize;
-            bytes_per_row * R
-        };
-        Self(Box::from(Vec::with_capacity(data_length)))
+        Self(BitBox::from(BitVec::with_capacity(R * C)))
     }
 }
 
 impl<const R: usize, const C: usize> BitGlyph<R, C> {
     /// Create a new Glyph
     ///
-    pub fn new(data: &[u8]) -> Option<BitGlyph<R, C>> {
-        let data_length = {
-            let (rows, cols) = (R, C as f64);
-            let bytes_per_row = (cols as f64 / 8.0).ceil() as usize;
-            bytes_per_row * rows
-        };
-
-        if data.len() < data_length {
+    pub fn new(data: &BitSlice<u8, Lsb0>) -> Option<BitGlyph<R, C>> {
+        if data.len() < (R * C) {
             None
         } else {
-            Some(Self(Box::from(data)))
+            Some(Self(BitBox::from(data)))
         }
-    }
-
-    /// Return the number of bytes needed to represent all the bits in a row.
-    ///
-    /// # Safety
-    /// The result of ceil cannot be NaN, infinite or negative.
-    /// As the input is a usize / 8, we know this is the case.
-    ///
-    fn bytes_per_row() -> usize {
-        unsafe { (C as f64 / 8.0).ceil().to_int_unchecked() }
     }
 }
 
 impl<const R: usize, const C: usize> Display for BitGlyph<R, C> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.chunks(Self::bytes_per_row()).for_each(|row| {
-            row.iter().for_each(|byte| {
-                let test: std::string::String = (0..8)
-                    .map(|shift| if byte & (128 >> shift) != 0 { '█' } else { '░' })
-                    .collect();
-                write!(f, "{}", test).unwrap();
-            });
-            writeln!(f).unwrap();
+        self.0.chunks(C).for_each(|row| {
+            let row_string: String = row.iter().map(|bit| if *bit { "██" } else { "░░" }).collect();
+            writeln!(f, "{}", row_string).unwrap();
         });
         std::fmt::Result::Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use bitvec::prelude::*;
 use rust_binfont_utils::BitGlyph;
 
 fn main() {
     let glyph: BitGlyph<8, 8> = {
-        let r1 = 0b00011000;
-        let r2 = 0b01100110;
-        let r3 = 0b01100110;
-        let r4 = 0b01111110;
-        let r5 = 0b01100110;
-        let r6 = 0b01100110;
-        let r7 = 0b00000000;
-        let r8 = 0b11111111;
-        BitGlyph::new(&[r1, r2, r3, r4, r5, r6, r7, r8]).expect("Failed to create glpyh\n")
+        let glyph = bits![u8, Lsb0; 0, 0, 0, 1, 1, 0, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1];
+        BitGlyph::new(glyph).expect("Failed to create glpyh\n")
     };
 
     print!("{}", glyph);
